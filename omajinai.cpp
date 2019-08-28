8813bf17e04f53d9f1856705149de82a3fdf04c6
SpicA <SpicA@MacUser-no-MacBook-Air.local>
2019-08-28 19:03:44 +0900

    first github

---
+++ omajinai.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include<iomanip>
+#include <map>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <utility>
+#include <queue>
+#define INF 1e9+7
+#define rep(i,n) for(int i=0;i<n;i++)
+#define NO cout<<"NO"<<endl;
+#define YES cout<< "YES"<<endl;
+#define No cout<< "No"<<endl;
+#define Yes cout<< "Yes"<<endl;
+#define all(a) a.begin(),a.end()
+#define P pair<int,int>
+using namespace std;
+typedef long long int ll;
+typedef unsigned long long int ull;
+
+//(int)'a'は97 (int)'A'は65 (int)'1'は49
+//おまじない
+
+const int maxn = 100000;
+
+int main(){
+    
+  return 0;
+}
